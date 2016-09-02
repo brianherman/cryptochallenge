d8fef02057d0c95cffe0683b27d6b4d52ee424cc
Brian Herman <brianherman@gmail.com>
2016-09-02 18:40:54 -0500

    Got it to work! (base64)

--- cryptochallenge/Source.cpp
+++ cryptochallenge/Source.cpp
@@ -1,34 +1,38 @@
 #include <string>
 #include <iostream>
-
-std::string base64_encode(std::string s) 
+char* CODES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+std::string base64_encode(char* in, int length) 
 {
 
-		std::string base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-		unsigned int c = s.length() % 3;
-		std::string p = "";
-		std::string r;
-		if (c > 0) {
-				for (; c < 3; c++) {
-						p += '=';
-						s += "\0";
+		std::string out;
+		int b;
+		for (int i = 0; i < length; i += 3) {
+				b = (in[i] & 0xFC) >> 2;
+				out += CODES[b];
+				b = (in[i] & 0x03) << 4;
+				if (i + 1 < length) {
+						b |= (in[i + 1] & 0xF0) >> 4;
+						out += CODES[b];
+						b = (in[i + 1] & 0x0F) << 2;
+						if (i + 2 < length) {
+								b |= (in[i + 2] & 0xC0) >> 6;
+								out += CODES[b];
+								b = (in[i + 2] & 0x3F);
+								out += CODES[b];
+						}
+						else {
+								out += CODES[b];
+								out += "=";
+						}
+				}else {
+						out += CODES[b];
+						out += "==";
 				}
 		}
-		for (c = 0; c < s.length(); c += 3) {
-				if (c > 0 && (c / 3 * 4) % 76 == 0) {
-						r += "\r\n";
-				}
-				int n = (base64chars[c] << 16) + (base64chars[c+1] << 8) + (base64chars[c+2]);
-				int n0 = (n >> 18) & 63;
-				int n1 = (n >> 12) & 63;
-				int n2 = (n >> 6) & 63;
-				int n3 = n & 63;
-				r += base64chars[n0] + base64chars[n1] + base64chars[n2] + base64chars[n3];
-				std::cout << r;
-		}
-		return r.substr(0, r.length() - p.length()) + p;
-
+		return out;
 }
 int main() {
-		std::cout << base64_encode("Man is distinguished, not only by his reason, but by this singular passion from\nother animals, which is a lust of the mind, that by a perseverance of delight\nin the continued and indefatigable generation of knowledge, exceeds the short\nvehemence of any carnal pleasure.");
+		char* s = "Man is distinguished, not only by his reason, but by this singular passion from\nother animals, which is a lust of the mind, that by a perseverance of delight\nin the continued and indefatigable generation of knowledge, exceeds the short\nvehemence of any carnal pleasure.";
+		std::cout << base64_encode(s, strlen(s));
+		
 }
